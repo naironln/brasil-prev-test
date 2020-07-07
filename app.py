ba80690d15b76973f1a0d3812c3ef787440d08ac
Nairon Lins Nogueira <naironxp@gmail.com>
2020-07-07 17:54:17 -0300

    products

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Api(app=flask_app,
           title="Cool t-shirt store",
           description="Manage store products" )
 
-logged_in = False
 
 users_name_space = app.namespace('Users', description='Main API')
 
@@ -35,23 +34,23 @@ users_list = {}
 @users_name_space.route("/")
 class Users(Resource):
 
-    @app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
-             params={ 'id': 'Specify the user id' })
+    # @app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
+    #          params={ 'id': 'Specify the user id' })
     
-    def get(self, id):
-        try:
-            user = users_list[id]
-            return {
-                "status": "User retrived",
-                "name": user['name'],
-                "email": user['email']
-            }
+    # def get(self, id):
+    #     try:
+    #         user = users_list[id]
+    #         return {
+    #             "status": "User retrived",
+    #             "name": user['name'],
+    #             "email": user['email']
+    #         }
 
-        except KeyError as e:
-            users_name_space.abort(500, e.__doc__, status="Unable to retrieve", statusCode="500")
+    #     except KeyError as e:
+    #         users_name_space.abort(500, e.__doc__, status="Unable to retrieve", statusCode="500")
         
-        except Exception as e:
-            users_name_space.abort(400, e.__doc__, status="Unable to retrieve", statusCode="400")
+    #     except Exception as e:
+    #         users_name_space.abort(400, e.__doc__, status="Unable to retrieve", statusCode="400")
 
     @app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' })
     @app.expect(user_model)      
@@ -62,18 +61,17 @@ class Users(Resource):
 
             return {
                 "status": "New user registered",
-                "name": users_list.get(email).get(email)
+                "user": users_list.get(email)['email']
             }
 
         except KeyError as e:
-            print(e)
             users_name_space.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
         
         except Exception as e:
-            print(e)
             users_name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
     
 
+LOGGED_IN = False
 @users_name_space.route("/login")
 class Login(Resource):
 
@@ -86,68 +84,97 @@ class Login(Resource):
 
             if user['password'] != request.json.get('password'):
                 raise KeyError
-            
-            logged_in = True
+            LOGGED_IN = True
             return {
                 "status": "Loged in"
             }
 
     
         except KeyError as e:
-            print(e)
             users_name_space.abort(401, e.__doc__, status = "unabble to authenticate", statusCode = "401")
         
         except Exception as e:
-            print(e)
             users_name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
 
 
 
 
-# products_name_space = app.namespace('Products', description='Main API')
+products_name_space = app.namespace('Products', description='Main API')
 
-# model = app.model("T-shirt Model",
-#                   {'name': fields.String(required=True,
-#                                          description="Name of the user",
-#                                          help="Name cannot be blank.")})
+product_model = app.model("T-shirt Model",
+                          {'name': fields.String(required=True,
+                                                 description="Name of the product",
+                                                 help="Name cannot be blank."),
+                           'color': fields.String(required=True,
+                                                 description="Color of the product",
+                                                 help="Color cannot be blank."),
+                           'id': fields.Integer(required=True,
+                                                 description="Id of the product",
+                                                 help="Id cannot be blank.")})
 
-# users_list = {}
+order_model = app.model("Order Model",
+                        {"id": fields.Integer(required=True,
+                                              description="Id of the product to be oredered",
+                                              help="id cannot be blank")})
 
-# @products_name_space.route("/<int:id>")
-# class Product(Resource):
+products_list = {'0': {'name': 'red cool t-shirt', 'color': 'red'},
+                 '1': {'name': 'blue and yellow lame t-shirt', 'color': 'blue and yellow'},}
 
-#     @app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
-#              params={ 'id': 'Specify the user id' })
+@products_name_space.route("/")
+class Products(Resource):
     
-#     def get(self, id):
-#         try:
-#             user = users_list[id]
-#             return {
-#                 "status": "User retrived",
-#                 "name": user
-#             }
-#         except KeyError as e:
-#             products_name_space.abort(500, e.__doc__, status="Unable to retrieve", statusCode="500")
+    def get(self):
+        try:
+            return {
+                "status": "User retrived",
+                "name": products_list
+            }
+        except KeyError as e:
+            products_name_space.abort(500, e.__doc__, status="Unable to retrieve", statusCode="500")
         
-#         except Exception as e:
-#             products_name_space.abort(400, e.__doc__, status="Unable to retrieve", statusCode="400")
+        except Exception as e:
+            products_name_space.abort(400, e.__doc__, status="Unable to retrieve", statusCode="400")
 
-#     @app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
-#              params={ 'id': 'Specify the user id' })
              
-#     @app.expect(model)      
-#     def post(self, id):
-#         try:
-#             users_list[id] = request.json['name']
-#             return {
-#                 "status": "New user registered",
-#                 "name": users_list[id]
-#             }
+    @app.expect(product_model)      
+    def post(self):
+        try:
+            product = request.json
+            print(product)
 
-#         except KeyError as e:
-#             print(e)
-#             products_name_space.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
+            if product['id'] in products_list:
+                return {
+                    "status": "cannot register new product",
+                    "message": "product's id is already in use"
+                }
+
+            users_list[product['id']] = product
+            return {
+                "status": "New product registered",
+                "product": product
+            }
+
+        except KeyError as e:
+            products_name_space.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
         
-#         except Exception as e:
-#             print(e)
-#             products_name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
+        except Exception as e:
+            products_name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
+
+
+@products_name_space.route("/place_order")
+class PlaceOrder(Resource):
+    @app.expect(order_model)      
+    def post(self):
+        try:
+
+            product = products_list.get(request.json.get('id'))
+            return {
+                "status": "Order placed",
+                "product": product
+            }
+
+        except KeyError as e:
+            products_name_space.abort(401, e.__doc__, status = "Unauthorized", statusCode = "401")
+        
+        except Exception as e:
+            products_name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
